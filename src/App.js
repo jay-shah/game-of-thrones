b5322674b7a6949cd59713de0a60ac5c8c2906a8
Jay Shah <jayshah14js@gmail.com>
2019-04-24 09:06:26 +0100

    initiall structure with semantic ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,122 @@
 import React from 'react';
 import './App.css';
+import { Grid, Header, Icon, Card, Divider } from 'semantic-ui-react'
+import Elliot from './images/jamie.jpg'
+import Daniel from './images/daniel.jpg'
 
 function App() {
   return (
-    <div className="App">
-      HELLO
-    </div>
+    <Grid divided columns={2} padded stackable >
+
+      <Grid.Column  >
+        <Grid.Row className="Title"  >
+          <Header as='h2' icon textAlign='center'   >
+            <Icon name='users' circular />
+            <Header.Content>Characters</Header.Content>
+          </Header>
+        </Grid.Row>
+        <Grid.Row  >
+          <Header as='h2' icon textAlign='left' className="Alive" color="green">
+            <Header.Content>ALIVE</Header.Content>
+          </Header>
+        </Grid.Row>
+        <Grid columns={6} padded stackable >
+          <Grid.Row  >
+            <Grid.Column>
+              <Card
+                image={Elliot}
+                header='Jamie Lannister'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Elliot}
+                header='Elliot Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Elliot}
+                header='Elliot Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Elliot}
+                header='Elliot Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Elliot}
+                header='Elliot Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Elliot}
+                header='Elliot Baker'
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Divider hidden />
+
+        <Grid.Row  >
+          <Header as='h2' icon textAlign='left' className="Alive" color="red">
+            <Header.Content>DEAD</Header.Content>
+          </Header>
+        </Grid.Row>
+        <Grid divided columns={5} padded stackable >
+          <Grid.Row  >
+            <Grid.Column>
+              <Card
+                image={Daniel}
+                header='Daniel Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Daniel}
+                header='Daniel Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Daniel}
+                header='Daniel Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Daniel}
+                header='Daniel Baker'
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Card
+                image={Daniel}
+                header='Daniel Baker'
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+      </Grid.Column>
+
+
+      <Grid.Column >
+        <Grid.Row className="Title"  >
+          <Header as='h2' icon textAlign='center'  >
+            <Icon name='users' circular />
+            <Header.Content>People</Header.Content>
+          </Header>
+        </Grid.Row>
+      </Grid.Column>
+
+
+    </Grid>
+
   );
 }
 
