576ee6ed04abeff5320d5be5b58d09d35e0d5b61
Jay Shah <jayshah14js@gmail.com>
2019-05-12 14:54:43 +0100

    now uses json file to read characters

--- src/App.js
+++ src/App.js
@@ -1,94 +1,104 @@
-import React from 'react';
 import './App.css';
-import { Grid, Header, Icon, Card, Divider, Table, Image } from 'semantic-ui-react'
+import { Grid, Header, Divider, Table, Image } from 'semantic-ui-react'
 import Elliot from './images/jamie.jpg'
 import Daniel from './images/daniel.jpg'
 import GOT from './images/got.png'
 import GOT1 from './images/got1.png'
 import CharacterCard from './components/CharacterCard'
 import PlayerCard from './components/PlayerCard'
+import React, { Component } from 'react'
+import Characters from './data/characters.json'
+
+export default class App extends Component {
+
+  getAliveCharacters = () => {
+
+    const alive = Object.keys(Characters).map((name) => {
+      if (Characters[name]["alive"]) {
+        return (<CharacterCard image={Elliot} name={name} />)
+      }
+    })
+    return alive
+  }
+
+  getDeadCharacters = () => {
+
+    const dead = Object.keys(Characters).map((name) => {
+      if (!Characters[name]["alive"]) {
+        return (<CharacterCard image={Daniel} name={name} />)
+      }
+    })
+    return dead
+  }
+
+  render() {
+
+    return (
+      <Grid divided columns={2} padded stackable>
+        <Grid.Column  >
+          <Grid.Row className="Title" centered >
+            <Image src={GOT} rounded size='medium' centered />
+
+          </Grid.Row>
+          <Grid.Row  >
+            <Header as='h2' icon textAlign='left' className="Alive" color="green">
+              <Header.Content>ALIVE</Header.Content>
+            </Header>
+          </Grid.Row>
+          <Grid padded stackable >
+            {this.getAliveCharacters()}
+          </Grid>
+          <Divider hidden />
+          <Grid.Row  >
+            <Header as='h2' icon textAlign='left' className="Alive" color="red">
+              <Header.Content>DEAD</Header.Content>
+            </Header>
+          </Grid.Row>
+          <Grid columns={5} padded stackable >
+            {this.getDeadCharacters()}
+          </Grid>
+
+        </Grid.Column>
 
 
-function App() {
-  return (
-    <Grid divided columns={2} padded stackable  >
+        <Grid.Column >
+          <Grid.Row className="Title"  >
+            <Image src={GOT1} rounded size='medium' centered />
+          </Grid.Row>
 
-      <Grid.Column  >
-        <Grid.Row className="Title" centered >
-          <Image src={GOT} rounded size='medium' centered />
+          <Grid>
+            <Grid.Column width={1}>
 
-        </Grid.Row>
-        <Grid.Row  >
-          <Header as='h2' icon textAlign='left' className="Alive" color="green">
-            <Header.Content>ALIVE</Header.Content>
-          </Header>
-        </Grid.Row>
-        <Grid padded stackable >
-          <CharacterCard image={Elliot} />
-          <CharacterCard image={Elliot} />
-          <CharacterCard image={Elliot} />
-          <CharacterCard image={Elliot} />
-          <CharacterCard image={Elliot} />
-          <CharacterCard image={Elliot} />
-        </Grid>
-        <Divider hidden />
-        <Grid.Row  >
-          <Header as='h2' icon textAlign='left' className="Alive" color="red">
-            <Header.Content>DEAD</Header.Content>
-          </Header>
-        </Grid.Row>
-        <Grid columns={5} padded stackable >
-          <CharacterCard image={Daniel} episode={1} />
-          <CharacterCard image={Daniel} episode={1} />
-          <CharacterCard image={Daniel} episode={1} />
-          <CharacterCard image={Daniel} episode={1} />
-          <CharacterCard image={Daniel} episode={4} />
-          <CharacterCard image={Daniel} episode={6} />
-        </Grid>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <Table size='small' >
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Position</Table.HeaderCell>
+                    <Table.HeaderCell>Picture Perfect</Table.HeaderCell>
+                    <Table.HeaderCell>Name</Table.HeaderCell>
+                    <Table.HeaderCell>Points</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
 
-      </Grid.Column>
+                <Table.Body>
+                  <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
+                  <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
+                  <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
+                  <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
+                </Table.Body>
+              </Table>
+            </Grid.Column>
+            <Grid.Column width={1}>
+
+            </Grid.Column>
+          </Grid>
+
+        </Grid.Column>
 
 
-      <Grid.Column >
-        <Grid.Row className="Title"  >
-          <Image src={GOT1} rounded size='medium' centered />
-        </Grid.Row>
+      </Grid >
 
-        <Grid>
-          <Grid.Column width={1}>
-
-          </Grid.Column>
-          <Grid.Column width={14}>
-            <Table size='small' >
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Position</Table.HeaderCell>
-                  <Table.HeaderCell>Picture Perfect</Table.HeaderCell>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                  <Table.HeaderCell>Points</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-
-              <Table.Body>
-                <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
-                <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
-                <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
-                <PlayerCard image={require('./images/jamie.jpg')} name='Jay Shah' position='1' points='15' />
-              </Table.Body>
-            </Table>
-          </Grid.Column>
-          <Grid.Column width={1}>
-
-          </Grid.Column>
-        </Grid>
-
-      </Grid.Column>
-
-
-    </Grid >
-
-  );
+    );
+  }
 }
-
-
-export default App;
--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Grid, Card, Image } from 'semantic-ui-react'
 
 
-const CharacterCard = ({ image, episode }) => {
+const CharacterCard = ({ image, episode, name }) => {
     return (
-        <Grid.Column mobile={4} tablet={8} computer={3}>
+        <Grid.Column mobile={4} tablet={8} computer={4}>
             <Card centered>
                 <Image src={image} />
                 <Card.Content>
-                    <Card.Header>Jamie Lannister</Card.Header>
+                    <Card.Header>{name}</Card.Header>
                     {episode ? <Card.Meta> <span >Episode {episode}</span></Card.Meta> : null}
                 </Card.Content>
             </Card>
