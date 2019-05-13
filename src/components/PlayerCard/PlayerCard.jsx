6256be52d95fabf3c18550a752c98c2c659615e3
Jay Shah <jayshah14js@gmail.com>
2019-05-13 23:13:20 +0100

    added modals on click table cell

--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
-import { Table, Image, Label, Header } from 'semantic-ui-react'
+import styles from './PlayerCard.module.css'
+import { Table, Image, Label, Header, Modal } from 'semantic-ui-react'
 
 const PlayerCard = ({ image, position, name, points, ribbonColor }) => {
+
+    const tableRow = () => {
+        return (
+            <Table.Row className={styles.tablerow}>
+                <Table.Cell width={2}>
+                    <Label ribbon color={ribbonColor}>{position}</Label>
+                </Table.Cell>
+                <Table.Cell width={3}> <Image src={image} size='tiny' circular /></Table.Cell>
+                <Table.Cell width={5}>
+                    <Header as='h2'>
+                        {name}
+                    </Header>
+                </Table.Cell>
+                <Table.Cell width={3}>
+                    <Header as='h2'>
+                        {points}
+                    </Header></Table.Cell>
+            </Table.Row>
+        )
+    }
+
+
     return (
-        <Table.Row>
-            <Table.Cell width={2}>
-                <Label ribbon color={ribbonColor}>{position}</Label>
-            </Table.Cell>
-            <Table.Cell width={3}> <Image src={image} size='tiny' circular /></Table.Cell>
-            <Table.Cell width={5}>
-                <Header as='h2'>
-                    {name}
-                </Header>
-            </Table.Cell>
-            <Table.Cell width={3}>
-                <Header as='h2'>
-                    {points}
-                </Header></Table.Cell>
-        </Table.Row>
+        <Modal trigger={tableRow()}>
+            <Modal.Header>{name}</Modal.Header>
+            <Modal.Content image>
+                <Image wrapped size='medium' src={image} />
+                <Modal.Description>
+                    <Header>Default Profile Image</Header>
+                    <p>We've found the following gravatar image associated with your e-mail address.</p>
+                    <p>Is it okay to use this photo?</p>
+                </Modal.Description>
+            </Modal.Content>
+        </Modal>
+
     )
 }
 
