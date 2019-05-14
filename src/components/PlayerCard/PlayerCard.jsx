cd9c13270aefaea735a6d075f50901dcc7b7281e
Jay Shah <jayshah14js@gmail.com>
2019-05-14 23:13:25 +0100

    added corrections on bonus

--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './PlayerCard.module.css'
 import Players from '../../data/players'
+import Characters from '../../data/characters'
 import { Table, Image, Label, Header, Modal, List, Icon, Divider } from 'semantic-ui-react'
 
 const PlayerCard = ({ image, position, name, points, ribbonColor }) => {
@@ -32,9 +33,14 @@ const PlayerCard = ({ image, position, name, points, ribbonColor }) => {
                     <List.Item className={styles.listitem}>
                         <List.Content>
                             <Image src={require('../../images/characters/' + character + '.jpg')} rounded size='tiny' />
-                            <Header className={styles.alive} textAlign='center' as='h3'>ALIVE</Header>
+                            <Header className={styles.alive} textAlign='center' as='h3'> ALIVE</Header>
+                            {Players[name]["characters"][character]['alive'] === Characters["characters"][character]["alive"] ?
+                                <Header className={styles.alive} textAlign='center'> <Icon name='check' /></Header> :
+                                <Header className={styles.dead} textAlign='center'> <Icon name='close' /></Header>
+                            }
+
                         </List.Content>
-                    </List.Item>
+                    </List.Item >
                 )
             }
         })
@@ -48,6 +54,10 @@ const PlayerCard = ({ image, position, name, points, ribbonColor }) => {
                         <List.Content>
                             <Image src={require('../../images/characters/' + character + '.jpg')} rounded size='tiny' />
                             <Header className={styles.dead} textAlign='center' as='h3'>DEAD {Players[name]["characters"][character]['episode'] ? Players[name]["characters"][character]['episode'] : null}</Header>
+                            {Players[name]["characters"][character]['alive'] === Characters["characters"][character]["alive"] ?
+                                <Header className={styles.alive} textAlign='center'> <Icon name='check' /></Header> :
+                                <Header className={styles.dead} textAlign='center'> <Icon name='close' /></Header>
+                            }
                         </List.Content>
                     </List.Item>
                 )
@@ -82,27 +92,49 @@ const PlayerCard = ({ image, position, name, points, ribbonColor }) => {
                         <List.Content>
                             <Header as='h2'>
                                 <Icon name='child' />
-                                <Header.Content>Is Daenerys pregnant? <span className={styles.bonus}>{Players[name]['bonus']["1"]}</span></Header.Content>
+                                <Header.Content>Is Daenerys pregnant?
+                                    <span className={styles.bonus}> {Players[name]['bonus']["1"]}</span>
+                                    {Players[name]["bonus"]["1"] === Characters["bonus"]["1"] ?
+                                        <span className={styles.alive} textAlign='center'> <Icon name='check' /></span> :
+                                        <span className={styles.dead} textAlign='center'> <Icon name='close' /></span>
+                                    }
+                                </Header.Content>
                             </Header>
                             <Divider hidden />
                             <Header as='h2'>
                                 <Icon name='eye' />
-                                <Header.Content>How many Dragons Left? <span className={styles.bonus}>{Players[name]['bonus']["2"]}</span></Header.Content>
+                                <Header.Content>How many Dragons Left? <span className={styles.bonus}>{Players[name]['bonus']["2"]}</span>
+                                    {Players[name]["bonus"]["2"] === Characters["bonus"]["2"] ?
+                                        <span className={styles.alive} textAlign='center'> <Icon name='check' /></span> :
+                                        <span className={styles.dead} textAlign='center'> <Icon name='close' /></span>
+                                    }</Header.Content>
                             </Header>
                             <Divider hidden />
                             <Header as='h2'>
                                 <Icon name='male' />
-                                <Header.Content>Which Clegane triumphs? <span className={styles.bonus}>{Players[name]['bonus']["3"]}</span></Header.Content>
+                                <Header.Content>Which Clegane triumphs? <span className={styles.bonus}>{Players[name]['bonus']["3"]}
+                                    {Players[name]["bonus"]["3"] === Characters["bonus"]["3"] ?
+                                        <span className={styles.alive} textAlign='center'> <Icon name='check' /></span> :
+                                        <span className={styles.dead} textAlign='center'> <Icon name='close' /></span>
+                                    }</span></Header.Content>
                             </Header>
                             <Divider hidden />
                             <Header as='h2'>
                                 <Icon name='chess knight' />
-                                <Header.Content>If the Night King Dies, who kills him? <span className={styles.bonus}>{Players[name]['bonus']["4"]}</span></Header.Content>
+                                <Header.Content>If the Night King Dies, who kills him? <span className={styles.bonus}>{Players[name]['bonus']["4"]}
+                                    {Players[name]["bonus"]["4"] === Characters["bonus"]["4"] ?
+                                        <span className={styles.alive} textAlign='center'> <Icon name='check' /></span> :
+                                        <span className={styles.dead} textAlign='center'> <Icon name='close' /></span>
+                                    }</span></Header.Content>
                             </Header>
                             <Divider hidden />
                             <Header as='h2'>
                                 <Icon name='wheelchair' />
-                                <Header.Content>Who holds the Iron Throne at the end? <span className={styles.bonus}>{Players[name]['bonus']["5"]}</span></Header.Content>
+                                <Header.Content>Who holds the Iron Throne at the end? <span className={styles.bonus}>{Players[name]['bonus']["5"]}
+                                    {Players[name]["bonus"]["5"] === Characters["bonus"]["5"] ?
+                                        <span className={styles.alive} textAlign='center'> <Icon name='check' /></span> :
+                                        <span className={styles.dead} textAlign='center'> <Icon name='close' /></span>
+                                    }</span></Header.Content>
                             </Header>
                         </List.Content>
                     </List.Item>
